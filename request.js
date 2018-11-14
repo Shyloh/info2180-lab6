5fe7cc0348e250541205137ad33ba0dd83672599
Shiloh Waite <shiloh.waite@mymona.uwi.edu>
2018-11-14 01:52:34 +0000

    Exercise 3

--- request.js
+++ request.js
@@ -2,25 +2,25 @@
 window.onload = function(){
     "use strict";
     let someId = document.getElementById("Search");
+    let tE = document.getElementById("sV");
     someId.addEventListener("click", doReach);
-};
+    let result=document.getElementById("result");
+
  function doReach () {
     let httpRequest = new XMLHttpRequest();
-    let url = "https://lab6---shyloh.c9users.io/request.php/request.php?q=definition";
     httpRequest.onreadystatechange = function(){
         if(httpRequest.readyState === 4){
             if(httpRequest.status ===200){
                 let response= (httpRequest.responseText.split("</h3>")); //splits term and tern defn
                 let div = document.createElement("div"); //used to remove html tags from php response
                 div.innerHTML = response[1]; //selecting only the definition.
-                alert(response[1]);
-            }else{
-                alert("There was an issue with the request.");
-            }
-        }
+                result.innerHTML = "<h2>Result</h2>"+response;
+        }}
     };
-    httpRequest.open("GET", url);
+    let url = "request.php?q="+ tE.value;
+    httpRequest.open("GET", url, true);
     httpRequest.send();
 }
-
+ 
+};
     
